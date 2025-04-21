e7d8ddd1bd9a1e30f2062427b5fccae9a161d1f7
wuye2325 <becktoby2325@gmail.com>
2025-04-21 11:17:49 +0800

    feat: 更新vite配置和路由设置，优化项目结构
    移除了vite.config.ts中的vite-plugin-singlefile插件，简化了配置。同时，将路由历史模式从createWebHashHistory更改为createWebHistory，以提升项目的可维护性和用户体验。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
@@ -10,7 +10,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, _from, savedPosition) {
     if (to.hash) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { fileURLToPath } from 'url';
-import { viteSingleFile } from "vite-plugin-singlefile"
-
-// 获取当前文件的目录名，兼容 ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [vue(), viteSingleFile()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
